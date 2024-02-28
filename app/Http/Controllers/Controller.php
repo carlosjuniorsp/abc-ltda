30ab3cb18aefecba1b16b528c13056333deb057b
Carlos <carlosdossantosjuniorsp@gmail.com>
2024-02-28 18:47:15 -0300

    installing laravel and swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,10 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Server(url="http://localhost/api"),
+ * @OA\Info(title="ABC LTDA", version="0.0.1")
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
